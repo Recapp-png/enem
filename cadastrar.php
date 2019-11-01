8d15dcfe7b9ab6b8f18f6da41d6259ba7f077d2e
Recapp-png <goldenecerebro@gmail.com>
2019-10-31 21:16:51 -0300

    segundocommit

--- cadastrar.php
+++ cadastrar.php
@@ -24,13 +24,13 @@ $u=new Usuário
 		</form>
 </div>
 <?php
-isset($_POST['nome']){
+if(isset($_POST['nome'])){
  $nome=addslashes($_POST['nome']);
  $telefone= addslashes($_POST['telefone']);
  $email= addslashes($_POST['email']);
  $senha= addslashes($_POST['senha']);
  $confirmarSenha= addslashes($_POST['confSenha']);
- if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha]))
+ if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha))
  {
  $u->conectar("projeto_login","localhost","root","");
  if( $u->msgErro==""){
@@ -46,13 +46,12 @@ isset($_POST['nome']){
 echo"Senha e confirmar senha são incompatíveis"; 
  }
  }else{
- echo="Erro:".$u->msgErro;
+ echo "Erro:".$u->msgErro;
  }
  }
  }else{
   echo"Preencha os campos que faltam!";
  }
-}
 ?>
 </body>
 </html>
---
+++ projeto_login.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 01-Nov-2019 às 01:15
+-- Versão do servidor: 10.4.6-MariaDB
+-- versão do PHP: 7.3.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `projeto_login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(40) DEFAULT NULL,
+  `telefone` varchar(35) DEFAULT NULL,
+  `email` varchar(40) DEFAULT NULL,
+  `senha` varchar(32) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
